889fbb3210e3b50b100a7aae67788f5ac0e3e363
Beppin00 <114412629+Beppin00@users.noreply.github.com>
2022-10-10 23:44:57 +0200

    Add files via upload

---
+++ assets/js/naming.js
@@ -0,0 +1,24 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto';
+var PetPreferito = 'coniglio';
+
+document.getElementById('prova').innerHTML = petPreferito;
+document.getElementById('prova2').innerHTML = PetPreferito;
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+//var 12 = 'numero';
+
+var primoNumero = 3;
+var secondoNumero = 5;
+console.log(primoNumero + secondoNumero);
+//document.write(primoNumero + secondoNumero);
